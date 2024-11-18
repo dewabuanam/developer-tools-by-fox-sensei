e04262ac1dc0b25feef0f1a5029072529335a755
Dewa Muharmadin <dewa_muharmadin@tsp.dev>
2024-11-18 14:48:01 +0700

    feat: add title

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
-// src/App.vue
 <script setup lang="ts">
 import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar'
 import { AppSidebar } from '@/components/ui/app-sidebar'
 import { useRoute } from 'vue-router'
 import { menuItems } from '@/constants/MenuItems'
+import { Label } from '@/components/ui/label'
+import { AppComponentGap } from '@/components/ui/app-component-gap'
 
 const route = useRoute()
 
@@ -21,6 +22,8 @@ const kebabToPascal = (str: string) => {
     <SidebarTrigger />
     <main class="content">
       <div class="router-page">
+        <Label for="title" class="title-label">{{ kebabToPascal(route.name) }}</Label>
+        <AppComponentGap size="large"/>
         <router-view />
       </div>
     </main>
@@ -37,5 +40,10 @@ const kebabToPascal = (str: string) => {
 .router-page {
   gap: 16px;
   padding: 16px;
+}
+
+.title-label {
+  font-size: 24px; /* Increase the label size */
+  font-weight: bold; /* Make the label bold */
 }
 </style>
