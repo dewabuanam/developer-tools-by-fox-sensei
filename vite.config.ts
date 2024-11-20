07e2fccb9b7dd6df10f889f38fd205d038f6552d
Dewa Muharmadin <dewa_muharmadin@tsp.dev>
2024-11-20 10:38:15 +0700

    fix: set base / as default

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  base: "/developer-tools-by-fox-sensei/",
+  base: "/",
   css: {
     postcss: {
       plugins: [tailwind(), autoprefixer()],
